36d901936386517535ae4d983715b2941ad89d93
leonardo.garcia <leonardo.garcia@targetwork.com.br>
2022-03-26 17:43:07 -0300

    Ajustes de aparência rodapé e cabeça

--- php/cabeca.php
+++ php/cabeca.php
@@ -4,15 +4,17 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/all.css">
-        <link rel="stylesheet" href="css/cabeca.css">
+        <link rel="stylesheet" href="../css/all.css">
+        <link rel="stylesheet" href="../css/cabeca.css">
         <title>FL Software - Mini Utilidades - Cabeca</title>
     </head>
     <body>
-        <?php
-            include "usuarioLogado.php";
-        ?>
-        <br/>
-        <a href="../index.html" target="_parent">Logout</a>
+        <p>
+            <?php
+                include "usuarioLogado.php";
+            ?>
+            <br/>
+            <a href="../index.html" target="_parent">Logout</a>
+        </p>
     </body>
 </html>
