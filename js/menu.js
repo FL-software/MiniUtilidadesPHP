a3ad0badd4799dc421eab0f5072caa5d3b788d43
leonardo.garcia <leonardo.garcia@targetwork.com.br>
2022-03-26 21:43:49 -0300

    seta animada no grupo menu

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,19 @@
 $("#calculadoras_grupo").click(function(){
     $("#calculadoras_itens").toggleClass("exibirSubMenu")
+    $("#calculadoras_seta").toggleClass("gira")
 })
 
 $("#lampadas_grupo").click(function(){
     $("#lampadas_itens").toggleClass("exibirSubMenu")
+    $("#lampadas_seta").toggleClass("gira")
 })
 
 $("#jogos_grupo").click(function(){
     $("#jogos_itens").toggleClass("exibirSubMenu")
+    $("#jogos_seta").toggleClass("gira")
 })
 
 $("#sistemas_grupo").click(function(){
     $("#sistemas_itens").toggleClass("exibirSubMenu")
+    $("#sistemas_seta").toggleClass("gira")
 })
