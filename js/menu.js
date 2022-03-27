09651d2c1a9ae96e6e138ff8ddbfbad3cadb124f
leonardo.garcia <leonardo.garcia@targetwork.com.br>
2022-03-26 21:31:35 -0300

    Criação de submenus

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+$("#calculadoras_grupo").click(function(){
+    $("#calculadoras_itens").toggleClass("exibirSubMenu")
+})
+
+$("#lampadas_grupo").click(function(){
+    $("#lampadas_itens").toggleClass("exibirSubMenu")
+})
+
+$("#jogos_grupo").click(function(){
+    $("#jogos_itens").toggleClass("exibirSubMenu")
+})
+
+$("#sistemas_grupo").click(function(){
+    $("#sistemas_itens").toggleClass("exibirSubMenu")
+})
